6d0ffc42bdca23d8803b6186dbf3853ce6ee8911
GoshanX <goshanx3@gmail.com>
2018-10-08 22:36:57 +0300

    Add project

---
+++ BLab_2/BLab_2/CodeFile1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+class Program
+{
+    static int func(string s1, string s2, int pos = -1)
+    {
+      
+        int i = 0;
+        int count = -1;
+        
+        while (i != -1)
+        {
+            i = s1.IndexOf(s2, pos + 1); 
+            pos = i;
+            count++;
+        }
+        
+        
+     
+        return count;
+    }
+    static void Main(string[] args)
+    {
+    
+        int size = 10000,l,count,max_count=1;
+        string s1;
+        List<int> result= new List<int>();
+        string[] tmp = new string[size];
+
+        s1 = Console.ReadLine();
+        Console.WriteLine();
+        l = int.Parse(Console.ReadLine());
+
+       
+        for (int i = 0; i < s1.Length; ++i) 
+            {
+                if (s1.Length - i > l - 1)
+                {
+                    tmp[i] = s1.Substring(i, l);
+
+                    if (tmp[i].Length == l)
+                    {
+                        count = func(s1, tmp[i], i);
+
+                        if (count == max_count)
+                        {
+                            result.Add(i);
+
+                        }
+                        if (count > max_count)
+                        {
+                            max_count = count;
+                            result.Clear();
+                            result.Insert(0, i);
+                        }
+                    }
+                }
+            }
+
+        foreach (int i in result)
+        {
+            Console.WriteLine(tmp[i]);
+            Console.WriteLine();
+        }
+ 
+    }
+}
---
+++ BLab_3/BLab_3/CodeFile1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s1;
+        StringBuilder sb = new StringBuilder();
+        s1 = Console.ReadLine();
+        char[] sReverse = s1.ToCharArray();
+        Array.Reverse(sReverse);
+        s1 = new string(sReverse);
+       for (int i = 0; i < s1.Length; i++)
+        {
+            
+            if(s1[i]=='T')
+            {
+
+                sb.Append("A");
+            }
+            if (s1[i] == 'A')
+            {
+
+                sb.Append("T");
+            }
+            if (s1[i] == 'C')
+            {
+
+                sb.Append("G");
+            }
+            if (s1[i] == 'G')
+            {
+
+                sb.Append("C");
+            }
+
+        }
+        s1 = sb.ToString();
+        
+      Console.WriteLine(s1);
+        Console.Read();
+    }
+}
